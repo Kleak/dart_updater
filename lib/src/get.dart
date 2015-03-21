17bb90d2b482fa1713e275771e8c6f7cafdd1616
Segaud Kevin <segaud.kevin@gmail.com>
2015-03-21 17:22:09 +0100

    Add copyright and name in the licence

--- lib/src/get.dart
+++ lib/src/get.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2015, Kevin Segaud. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 part of dart_updater;
 
 Future<String> getLastVersionAndRevisionInfo(String channel) async {
--- lib/src/zip.dart
+++ lib/src/zip.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2015, Kevin Segaud. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 part of dart_updater;
 
 void unZip(File file, String out_path) {
